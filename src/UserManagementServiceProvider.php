e4e8d40e221ae90e9cab22f6aec337543427e1c4
Labrock <vladimirpeshkov5@yandex.ru>
2023-12-17 17:24:06 +0300

    init commit

---
+++ src/UserManagementServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Aweram\UserManagement;
+
+use Illuminate\Support\ServiceProvider;
+
+class UserManagementServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+
+    }
+
+    public function register(): void
+    {
+
+    }
+}
