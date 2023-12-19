91d2bc97b64b5be596adc07f2638beaa4bca3712
Labrock <vladimirpeshkov5@yandex.ru>
2023-12-19 09:21:47 +0300

    adaptive search form

--- src/resources/views/admin/includes/user-search.blade.php
+++ src/resources/views/admin/includes/user-search.blade.php
@@ -1,11 +1,11 @@
 <div class="flex justify-between mb-indent-half">
-    <div class="flex space-x-indent-half">
+    <div class="flex flex-col space-y-indent-half md:flex-row md:space-x-indent-half md:space-y-0">
         <input type="text" aria-label="Имя" placeholder="Имя" id="name" class="form-control" wire:model.live="searchName">
         <input type="text" aria-label="E-mail" placeholder="E-mail" id="email" class="form-control" wire:model.live="searchEmail">
         <button type="button" class="btn btn-outline-primary" wire:click="clearSearch">{{ __("Clear") }}</button>
     </div>
     <div>
-        <button type="button" class="btn btn-primary px-btn-x-ico lg:px-btn-x"
+        <button type="button" class="btn btn-primary px-btn-x-ico lg:px-btn-x ml-indent-half"
                 wire:click="showCreate">
             <x-tt::ico.circle-plus />
             <span class="hidden lg:inline-block pl-btn-ico-text">{{ __("Add") }}</span>
